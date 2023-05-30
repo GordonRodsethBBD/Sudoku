4cd5b679c7f157538bc0abe3d681c9d51282113c
shagan-vodacom <124994196+shagan-vodacom@users.noreply.github.com>
2023-05-30 04:40:05 +0200

    Fix DB init queries, adds register form

--- app/controllers/initDBController.js
+++ app/controllers/initDBController.js
@@ -2,117 +2,77 @@ const { Connection, Request, TYPES } = require('tedious');
 const {connection, connectToDatabase} = require('./dbController');
 
 
-const queryCreateTables = `CREATE TABLE tblGame (
-    id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
-    userId INT NOT NULL,
-    duration INT NOT NULL,
-    levelId INT NOT NULL,
-    FOREIGN KEY (levelId) REFERENCES tblLevel(id),
-    FOREIGN KEY (userId) REFERENCES tblUser(id)
-  );
-  
+const queryCreateTables =
+`
+    CREATE TABLE tblUser (
+        id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+        username VARCHAR(100) NOT NULL,
+        emailHash VARCHAR(150) NOT NULL,
+        passwordHash VARCHAR(255) NOT NULL
+    );
+
+    CREATE TABLE tblLevel (
+        id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+        hiddenSquares INT NOT NULL,
+        title VARCHAR(30) NOT NULL
+      );
+
+    CREATE TABLE tblGame (
+        id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+        userId INT NOT NULL,
+        levelId INT NOT NULL,
+        isDone BOOL NOT NULL,
+        duration INT NOT NULL,
+        FOREIGN KEY (levelId) REFERENCES tblLevel(id),
+        FOREIGN KEY (userId) REFERENCES tblUser(id)
+    );
+
   CREATE TABLE tblLeaderboard (
-    id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
-    gameId INT,
-    levelId INT,
-    userId INT,
-    FOREIGN KEY (gameId) REFERENCES tblGame(id),
-    FOREIGN KEY (levelId) REFERENCES tblLevel(id),
-    FOREIGN KEY (userId) REFERENCES tblUser(id)
-  );
-  
-  CREATE TABLE tblUser (
-    id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
-    username VARCHAR(100) NOT NULL,
-    emailHash VARCHAR(150) NOT NULL,
-    passwordHash VARCHAR(255) NOT NULL
-  );
-  
-  CREATE TABLE tblLevel (
-    id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
-    hiddenSquares INT NOT NULL,
-    title VARCHAR(30) NOT NULL
+        id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+        gameId INT,
+        levelId INT,
+        userId INT,
+        FOREIGN KEY (gameId) REFERENCES tblGame(id),
+        FOREIGN KEY (levelId) REFERENCES tblLevel(id),
+        FOREIGN KEY (userId) REFERENCES tblUser(id)
   );
   `
 
-const queryPopulateDB = `INSERT INTO tblGame (userId, duration, levelId)
+const queryPopulateDB =
+`
+INSERT INTO tblUsers (username, emailHash, passwordHash)
 VALUES
-  (1, 30, 1),
-  (2, 45, 1),
-  (3, 60, 2),
-  (4, 35, 2),
-  (1, 50, 3),
-  (2, 55, 3),
-  (3, 40, 1),
-  (4, 65, 2),
-  (1, 70, 2),
-  (2, 25, 3);
+    ('rudolph', 'adfafasdfsdfa', 'sfaddafdafdaf'),
+    ('thabang', 'adfafasdfsdfa', 'sfaddafdafdaf'),
+    ('lucky', 'adfafasdfsdfa', 'sfaddafdafdaf');
 
-
-INSERT INTO tblLeaderboard (gameId, levelId, userId)
+INSERT INTO tblLevels (title, hiddenSquares)
 VALUES
-  (1, 1, 1),
-  (2, 1, 2),
-  (3, 2, 3),
-  (4, 2, 4),
-  (5, 3, 1),
-  (6, 3, 2),
-  (7, 1, 3),
-  (8, 2, 4),
-  (9, 2, 1),
-  (10, 3, 2);
+    ('Easy', 29),
+    ('Medium', 38),
+    ('Hard', 47),
+    ('Very hard', 56),
+    ('Insane', 65),
+    ('Inhuman', 74);
 
-INSERT INTO tblLeaderboard (gameId, levelId, userId)
+INSERT INTO tblGame (userId, levelId, isDone, duration )
 VALUES
-  (1, 1, 1),
-  (2, 1, 2),
-  (3, 2, 3),
-  (4, 2, 4),
-  (5, 3, 1),
-  (6, 3, 2),
-  (7, 1, 3),
-  (8, 2, 4),
-  (9, 2, 1),
-  (10, 3, 2);
-
-INSERT INTO tblLeaderboard (gameId, levelId, userId)
-VALUES
-  (1, 1, 1),
-  (2, 1, 2),
-  (3, 2, 3),
-  (4, 2, 4),
-  (5, 3, 1),
-  (6, 3, 2),
-  (7, 1, 3),
-  (8, 2, 4),
-  (9, 2, 1),
-  (10, 3, 2);
+    (1, 2, true, 5000),
+    (2, 1, false, 5000),
+    (2, 1, true, 5555);
 `
 
-const queryDropTables = `DECLARE @tableName NVARCHAR(128)
-DECLARE @dropTableSQL NVARCHAR(MAX) = ''
+const queryDropTables = `
+USE SudokuGame;
 
+IF OBJECT_ID('dbo.tblUsers', 'U') IS NOT NULL
+    DROP TABLE dbo.tblUsers;
 
-DECLARE tableCursor CURSOR FOR
-SELECT TABLE_NAME
-FROM INFORMATION_SCHEMA.TABLES
-WHERE TABLE_TYPE = 'BASE TABLE'
+IF OBJECT_ID('dbo.tblGame', 'U') IS NOT NULL
+    DROP TABLE dbo.tblGame;
 
-
-OPEN tableCursor
-FETCH NEXT FROM tableCursor INTO @tableName
-WHILE @@FETCH_STATUS = 0
-BEGIN
-    SET @dropTableSQL = @dropTableSQL + 'DROP TABLE ' + QUOTENAME(@tableName) + ';'
-
-    FETCH NEXT FROM tableCursor INTO @tableName
-END
-
-CLOSE tableCursor
-DEALLOCATE tableCursor
-
-
-EXECUTE sp_executesql @dropTableSQL;
+IF OBJECT_ID('dbo.tblLevel', 'U') IS NOT NULL
+    DROP TABLE dbo.tblLevel;
 
 `
 
@@ -161,7 +121,7 @@ function dropTables(){
     });
 };
 
-function tests() {
+function populate() {
     try {
         dropTables();
     } catch(e) {
@@ -169,12 +129,9 @@ function tests() {
     }
     createTables();
     populateTables();
-    UserFetchAll();
-    UserFetchSingle('user1');
-    GameFetchAll()
 };
 
 module.exports = {
-    tests
+    populate
 }
 
